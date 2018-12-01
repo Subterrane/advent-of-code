ee4ab6ac328e54b20d73569221d25802bb29bff6
Will Munslow <will@subterrane.com>
2018-12-01 13:45:55 -0800

    tiny refactor

--- index.mjs
+++ index.mjs
@@ -1,18 +1,17 @@
 import { input } from './input.mjs'
 
-const sumList = [];
-const dupList = []; 
+const sumList = [], dupList = [];
 
 const process = (start, sum) => {
     return input.split('\n').reduce((prev, cur) => {
         sum = parseInt(cur, 10) + prev;
         sumList.includes(sum) ? dupList.push(sum) : sumList.push(sum);
         return parseInt(cur, 10) + prev;
-    }, start); 
+    }, start);
 };
 
 let total = process(0);
-while(!dupList.length) total = process(total);
+while (!dupList.length) total = process(total);
 
 console.log(dupList[0]);
 console.log('end');
