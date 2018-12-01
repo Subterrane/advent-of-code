761c900ba9ca720de82a1d9c4a57b9406e3dbce7
Will Munslow <will@subterrane.com>
2018-12-01 13:47:29 -0800

    derp, return the var, don't reparse

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const process = (start, sum) => {
     return input.split('\n').reduce((prev, cur) => {
         sum = parseInt(cur, 10) + prev;
         sumList.includes(sum) ? dupList.push(sum) : sumList.push(sum);
-        return parseInt(cur, 10) + prev;
+        return sum;
     }, start);
 };
 
